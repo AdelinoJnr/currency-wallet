5650088533643cabc1641d252c302cb8f854eb11
AdelinoJnr <adelinojunior96@gmail.com>
2021-08-14 20:17:15 -0300

    [feat: page wallet investiments]

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,13 +1,45 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import Welcome from '../components/Welcome';
+import Investimentos from '../components/Investimentos';
 
 import { CurrencyContext } from '../store/Currency/currency';
+import { calculateBalanceValues, converteInNumber } from '../utils/functions';
 
 function Wallet() {
+  const [totalBalance, setTotalBalance] = useState(0);
+  const [lucro, setLucro] = useState(0);
   const { currency } = useContext(CurrencyContext);
+  const key = localStorage.getItem('investimentos');
+
+  useEffect(() => {
+    const updateBalance = () => {
+      const storage = key ? JSON.parse(key) : [];
+      const lucroCurrent = storage.reduce((acc, curr) => {
+        const inputCurrent = converteInNumber(curr.currentValue);
+        const price = converteInNumber(currency[curr.code].ask);
+        const totalPrice = inputCurrent * price;
+        return acc + totalPrice;
+      }, 0);
+      const data = calculateBalanceValues('totalValue', storage);
+      const calculate = data - lucroCurrent;
+      const total = calculate >= 0 ? `+${calculate.toFixed(2)}` : calculate.toFixed(2); 
+      setLucro(total);
+      setTotalBalance(data.toFixed(2));
+    };
+    updateBalance();
+  }, []);
+
+  const renderInvestments = () => {
+    const storage = key ? JSON.parse(key) : [];
+    return (
+      <section className="main-investimentos">
+        {storage.map((item, index)=> <Investimentos key={ index } investiment={item} currency={currency} />)}
+      </section>
+    );
+  };
 
   if (!currency) {
     return <Loading />;
@@ -30,8 +62,10 @@ function Wallet() {
       <section>
         <div className="content-balance">
           <span>Total Balance</span>
-          <p>100000</p>
+          <span>{lucro}</span>
+          <p>{`${totalBalance} BRL`}</p>
         </div>
+      {renderInvestments()}
       </section>
       <Footer />
     </>
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,9 +1,21 @@
 export const updateLocalStorage = (key, value) => {
   const chave = localStorage.getItem(key);
   const checkedStorage = chave ? JSON.parse(chave) : false;
+  console.log(checkedStorage);
   if (checkedStorage) {
-    localStorage.setItem(key, JSON.stringify([checkedStorage, value]));
+    localStorage.setItem(key, JSON.stringify([...checkedStorage, value]));
     return console.log('Ja tinha algo');
   }
-  localStorage.setItem(key, JSON.stringify(value));
+  localStorage.setItem(key, JSON.stringify([value]));
+};
+
+export const converteInNumber = (stringNumber) => {
+  return Number(stringNumber);
+};
+
+export const calculateBalanceValues = (key, array) => {
+  return array.reduce((acc, curr) => {
+    const value = converteInNumber(curr[key]);
+    return acc + value;
+  }, 0)
 };
