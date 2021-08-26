d29392b84cd24de98d1edf094de22115efc60e74
mallmann02 <leomallmann020302@gmail.com>
2021-08-25 22:42:39 -0300

    [feat: implement non-popular coins selector]
    display an selector that allows user to select other coin to track

--- src/components/Currencys.jsx
+++ src/components/Currencys.jsx
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import Select from 'react-select';
+import { currencysNames } from '../data';
 import { CurrencyContext } from '../store/Currency/currency';
 
 import CardCurrency from './CardCurrency';
@@ -6,14 +8,30 @@ import Loading from './Loading';
 
 function Currencys() {
   const { currencyCrypto, currencyPopular } = useContext(CurrencyContext);
+  const [ searchedCoins, setOtherCoins ] = useState([]);
 
-  const renderCurrencyCrypto = () => 
+  const options = currencyCrypto?.map((currency, index) => (
+    {value: currency, label: `${currencysNames[Object.keys(currency)]} (${Object.keys(currencysNames)[index]})`}
+  ));
+
+  const renderCurrencyCrypto = () =>
   currencyPopular.map((currency, index) => (
       <CardCurrency key={index} currency={currency} />
     ));
 
   return (
-    <section>
+    <section className="currencys-container">
+      <Select
+        className="coin-selector"
+        isLoading={(!currencyCrypto) === true}
+        onChange={(e) => setOtherCoins(searchedCoins.concat(e.value))}
+        options={options}
+      />
+      { (searchedCoins.length) !== 0
+        ? searchedCoins.map((coin, index) => (<CardCurrency key={index} currency={coin}/>))
+        : <p className="coin-selector-tip-text">Procure por uma meoda específica no seletor acima</p>
+      }
+      <h1>Most Popular Coins</h1>
       {!currencyPopular ? <Loading /> : renderCurrencyCrypto()}
     </section>
   );
