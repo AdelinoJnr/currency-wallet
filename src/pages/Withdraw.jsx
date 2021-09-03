0a856ba7dac63397edc41ce43aa9c113c415888a
AdelinoJnr <adelinojunior96@gmail.com>
2021-09-02 21:41:35 -0300

    [style: page sacar]

--- src/pages/Withdraw.jsx
+++ src/pages/Withdraw.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import formatar from 'formatar-valores';
 
 import Footer from '../components/Footer';
 import HeaderBack from '../components/HeaderBack';
@@ -8,22 +7,45 @@ import { currencyActivity } from '../utils/functions';
 
 function Withdraw() {
   const [valueWithdraw, setValueWithdraw] = useState('');
-  const [cpfTitular, setCpfTitular] = useState('');
-  const [phoneTitular, setPhoneTitular] = useState('');
 
   return (
     <>
-      <HeaderBack text="Saque" rota="/wallet" />
-      <h3>Saque agora</h3>
-      <p>quantidade que deseja sacar</p>
-      <input value={valueWithdraw} onChange={(ev) => setValueWithdraw(ev.target.value)} type="number" name="saque" id="saque" />
-      <input type="text" name="name" id="name" placeholder="Nome do titular" />
-      <input onChange={(ev) => setCpfTitular(ev.target.value)} value={formatar.cpfCnpj(cpfTitular)} type="text" name="cpf" id="cpf" placeholder="CPF do Titular" />
-      <input onChange={(ev) => setPhoneTitular(ev.target.value)} value={phoneTitular} type="text" name="phone" id="phone" placeholder="Telefone para contato" />
-      <input type="number" name="numCart" id="numCart" placeholder="Numero do cartão" />
-      <Link to="/">
-        <button onClick={() => currencyActivity(valueWithdraw, 'sacar')} disabled={valueWithdraw === ''} type="button">confirmar</button>
-      </Link>
+      <HeaderBack text="Sacar" rota="/wallet" />
+      <div className="content-saque">
+        <p>Quanto deseja sacar ?</p>
+        <input
+          value={valueWithdraw}
+          onChange={(ev) => setValueWithdraw(ev.target.value)}
+          type="number"
+          name="saque"
+          placeholder="Valor"
+        />
+      </div>
+
+      <form className="form-sacar">
+        <h4 className="title-3">Preencher os dados</h4>
+        <select name="banco" id="banco">
+          <option value="">-- Esolha seu banco --</option>
+          <option value="caixa">Caixa economica federal</option>
+          <option value="brasil">Banco do Brasil</option>
+          <option value="itau">Banco Itaú</option>
+          <option value="nubank">Nubank</option>
+        </select>
+        <input type="number" name="conta" placeholder="Número da conta" />
+        <input type="text" name="name" placeholder="Nome do Titular" />
+        <input type="text" name="telefone" placeholder="Telefone para contato" />
+        <Link className="content-btn" to="/">
+          <button
+            onClick={() => currencyActivity(valueWithdraw, 'sacar')}
+            disabled={valueWithdraw === ''}
+            type="button"
+            className="btn-acao btn-saque"
+          >
+            confirmar
+          </button>
+        </Link>
+      </form>
+
       <Footer />
     </>
   );
