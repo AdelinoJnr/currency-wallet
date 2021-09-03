e559528e2884d183047b10c576a4511f9371af09
AdelinoJnr <adelinojunior96@gmail.com>
2021-09-03 09:34:37 -0300

    [style: page cadastro]

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -4,36 +4,26 @@ import { Link } from 'react-router-dom';
 function Cadastro() {
   return (
     <main className="main-login">
-      <h3 className="title-login">Sign up</h3>
-      <label className="label-input" htmlFor="email">
-        <input type="email" id="email" className="inputs" placeholder="Digite seu email" />
-      </label>
+      <h3 className="title">Sign up</h3>
+      <input type="email" id="email" className="inputs" placeholder="Digite seu email" />
       <div className="content-full-name">
         <input className="input-name-user" type="text" id="firstname" placeholder="Digite seu nome" />
         <input className="input-name-user" type="text" id="lastname" placeholder="Digite seu sobrenome" />
       </div>
+      <input className="input-date" type="date" id="data" />
+      <input type="password" id="password" className="inputs" placeholder="Digite sua senha" />
+      <input type="password" id="password" className="inputs" placeholder="Digite sua senha novamente" />
 
-      <label className="label-input" htmlFor="data">
-        <input className="input-date" type="date" id="data" />
-      </label>
-
-      <label className="label-input" htmlFor="password">
-        <input type="password" id="password" className="inputs" placeholder="Digite sua senha" />
-      </label>
-      <label className="label-input" htmlFor="password">
-        <input type="password" id="password" className="inputs" placeholder="Digite sua senha novamente" />
-      </label>
       <label className="label-input" htmlFor="termos">
         <input className="checkbox-termos" type="checkbox" name="termos" id="termos" />
-        <span className="text-termos">
+        <p className="text-termos">
           Eu li e e aceito os
-          {' '}
           <Link to="/">Termos e condições </Link>
           de uso!
-        </span>
+        </p>
       </label>
-      <Link className="link-cadastrar-btn" to="/login">
-        <button className="btn-cadastrar btn-padrao" type="button">
+      <Link to="/login">
+        <button className="btn-acao btn-cadastrar" type="button">
           Cadastrar
         </button>
       </Link>
