9ac7f8fc8b10d7a7e71e512befd43b88cff409a6
AdelinoJnr <adelinojunior96@gmail.com>
2021-09-05 13:18:27 -0300

    [style: buttons metodos de pagamento]

--- src/pages/Deposit.jsx
+++ src/pages/Deposit.jsx
@@ -5,12 +5,11 @@ import Footer from '../components/Footer';
 import FormDeposit from '../components/FormDeposito';
 import FormBoleto from '../components/FormBoleto';
 import FormPix from '../components/FormPix';
+import MetodoPagemento from '../components/MetotoPagamento/indes';
 
 function Deposit() {
   const [valueDeposit, setValueDeposit] = useState('');
   const [metodoPagement, setMetodoPagament] = useState('');
-
-  const handleClick = ({ target: { innerHTML } }) => setMetodoPagament(innerHTML);
 
   const renderForm = () => {
     if (metodoPagement === 'Cartao') return <FormDeposit value={valueDeposit} />;
@@ -26,12 +25,7 @@ function Deposit() {
           <p>Quanto deseja depositar ?</p>
           <input value={valueDeposit} onChange={(ev) => setValueDeposit(ev.target.value)} type="number" name="valueDeposito" id="valueDeposito" />
         </div>
-        <div className="content-metodo-pagament">
-          <h3 className="title-3">Metodo de pagamento</h3>
-          <button onClick={handleClick} className="btn-cartao" type="button">Cartao</button>
-          <button onClick={handleClick} className="btn-boleto" type="button">Boleto</button>
-          <button onClick={handleClick} className="btn-pix" type="button">Pix</button>
-        </div>
+        <MetodoPagemento setMetodoPagament={setMetodoPagament} />
         {renderForm()}
       </section>
       <Footer />
